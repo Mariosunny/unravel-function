45e14bcc16be7917348f83aa61021a9c51a0d980
Tyler Hurson <glassplotful@gmail.com>
2018-09-27 14:27:47 -0500

    Fix non-reusable bug

--- src/unravel.js
+++ src/unravel.js
@@ -1,15 +1,38 @@
-import util from './util';
+/*
+Main source code for the NPM module unravel-function, by Tyler Hurson. unravel-function takes a function and spreads
+its arguments across a chain of functions to be lazily evaluated. See README.md for examples.
+
+Github Repo:
+https://github.com/Mariosunny/unravel-function
+ */
+
+import {getParameters, fillArray} from './util';
 
 const UNFILLED_ARG = Symbol();
 
-function unravel(func, params) {
-    let funcParameters = util.getParamters(func);
-    params = params || [];
+/**
+ * Returns an object where each parameter name of func is mapped to a function that expects a single argument and then
+ * returns another object, and so on until all arguments of func have been filled. On the last call in the chain, func
+ * is evaluated with the provided arguments and returned. eval() can be called at any point in the chain, and will
+ * evaluate func with the current arguments, with any remaining arguments set to undefined.
+ *
+ * @author Tyler Hurson
+ * @param {function} func The function to be evaluated at the end of the chain or when eval() is called
+ * @param {string[]=} [params=[]] A list of parameter names that override the parameter names of the function in the chain
+ * @returns {Object} The unraveled function chain
+ */
+export default function unravel(func, params) {
+    let funcParameters = getParameters(func);
+    let overrideParams = params || [];
+
+    validateFunc(func);
+    validateParams(overrideParams);
+
     return unravelRecursive(
         func,
         funcParameters,
-        params,
-        util.fillArray(funcParameters.length, UNFILLED_ARG),
+        overrideParams,
+        fillArray(funcParameters.length, UNFILLED_ARG),
         funcParameters.length
     );
 }
@@ -18,7 +41,7 @@ function unravelRecursive(func, functionParams, overrideParams, args, argsToGo) 
     if(argsToGo > 0) {
         let obj = {
             eval: function() {
-                return func.apply(null, normalizeArgs(args));
+                return callFunc(func, args);
             }
         };
 
@@ -26,8 +49,9 @@ function unravelRecursive(func, functionParams, overrideParams, args, argsToGo) 
             if(arg === UNFILLED_ARG) {
                 let paramName = getParamName(i, functionParams, overrideParams);
                 obj[paramName] = function (arg) {
-                    args[i] = arg;
-                    return unravelRecursive(func, functionParams, overrideParams, args, argsToGo - 1);
+                    let newArgs = args.slice(0);
+                    newArgs[i] = arg;
+                    return unravelRecursive(func, functionParams, overrideParams, newArgs, argsToGo - 1);
                 }
             }
         });
@@ -35,8 +59,12 @@ function unravelRecursive(func, functionParams, overrideParams, args, argsToGo) 
         return obj;
     }
     else {
-        return func.apply(null, normalizeArgs(args));
+        return callFunc(func, args);
     }
+}
+
+function callFunc(func, args) {
+    return func.apply(null, normalizeArgs(args));
 }
 
 function normalizeArgs(args) {
@@ -55,4 +83,17 @@ function getParamName(i, functionParams, overrideParams) {
     return functionParams[i];
 }
 
-export default unravel;
+function validateFunc(func) {
+    if(func === undefined) {
+        throw new Error("first argument required");
+    }
+    if(typeof func !== 'function') {
+        throw new Error("first argument must be a function")
+    }
+}
+
+function validateParams(params) {
+    if(!Array.isArray(params)) {
+        throw new Error("second argument must be an array");
+    }
+}
